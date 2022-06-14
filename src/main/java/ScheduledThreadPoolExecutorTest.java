e18805158d1d4d2981f3cdbc3ee07029e6953bb6
hukangkang <hukangkang@xiaomi.com>
2022-06-14 16:18:17 +0800

    线程池定时任务执行

--- src/main/java/ScheduledThreadPoolExecutorTest.java
+++ src/main/java/ScheduledThreadPoolExecutorTest.java
@@ -17,10 +17,12 @@ public class ScheduledThreadPoolExecutorTest {
     public void testDelayedTask() {
         System.out.println("开始测试时间" + LocalTime.now());
         count.increment();
+        // 已结束时间开始计算delay
         executor.scheduleWithFixedDelay(() -> {
             System.out.println("第" + count + "次开始时间: " + LocalTime.now());
             sleep(SECONDS, 10);
             System.out.println("第" + count + "次结束时间:" + LocalTime.now());
+            System.out.println("-----");
             count.increment();
         }, 1, 5, SECONDS);
     }
@@ -28,10 +30,12 @@ public class ScheduledThreadPoolExecutorTest {
     public void testRateTask() {
         System.out.println("开始测试时间" + LocalTime.now());
         count.increment();
+        // 如果执行周期到了，但是上一次执行没有完成，则等待上一次执行完成之后立即开始下一次执行
         executor.scheduleAtFixedRate(() -> {
             System.out.println("第" + count + "次开始时间" + LocalTime.now());
-            sleep(SECONDS, 10);
+            sleep(SECONDS, 7);
             System.out.println("第" + count + "次结束时间" + LocalTime.now());
+            System.out.println("-----");
             count.increment();
         }, 1, 5, SECONDS);
     }
@@ -45,7 +49,7 @@ public class ScheduledThreadPoolExecutorTest {
     }
 
     public static void main(String[] args) {
-//        new ScheduledThreadPoolExecutorTest().testDelayedTask();
+       // new ScheduledThreadPoolExecutorTest().testDelayedTask();
         new ScheduledThreadPoolExecutorTest().testRateTask();
     }
 }
